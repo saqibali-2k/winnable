14b092ef1446ad6c6ec251b02c1ffacce3062ecc
Filip <filipjovanovic1160@gmail.com>
2024-03-28 00:26:31 -0400

    Add some docstrings

--- training_data_collector.py
+++ training_data_collector.py
@@ -3,10 +3,20 @@ from random import choice, randint
 from pathlib import Path
 import json
 
-API_KEY = 'RGAPI-6e6ac8e6-63c0-4d2d-adab-61b1bb68ec5f'  # FOR TESTING ONLY, REMOVE LATER
+API_KEY = ''
 
 
 def fetch_random_match_id(lol_watcher, region, queue, rank, division, page):
+    """Fetch a random match ID based on the given parameters
+
+    :param lol_watcher: LolWatcher instance
+    :param region: String indicating match region
+    :param queue: String indicating match queue type
+    :param rank: String indicating match rank
+    :param division: String indicating the division of the match rank
+    :param page: Integer indicating the page number to fetch the match from
+    :return: Random match ID
+    """
     rank_summoners = lol_watcher.league.entries(region=region, queue=queue, tier=rank, division=division, page=page)
     summoner = choice(rank_summoners)
     summoner_id = summoner['summonerId']
@@ -19,15 +29,44 @@ def fetch_random_match_id(lol_watcher, region, queue, rank, division, page):
 
 
 def fetch_match_info(lol_watcher, region, match_id):
+    """Fetch match info from a given match ID
+
+    :param lol_watcher: LolWatcher instance
+    :param region: String indicating match region
+    :param match_id: String indicating match ID
+    :return:
+    """
     return lol_watcher.match.by_id(region=region, match_id=match_id)
 
 
 def fetch_match_timeline(lol_watcher, region, match_id):
+    """Fetch match timeline from a given match ID
+
+    :param lol_watcher: LolWatcher instance
+    :param region: String indicating match region
+    :param match_id: String indicating match ID
+    :return:
+    """
     return lol_watcher.match.timeline_by_match(region=region, match_id=match_id)
 
 
 def generate_data(region, queue, ranks, divisions, min_page,
-                  max_page, num_samples, match_info_filepath, match_timeline_filepath):
+                  max_page, num_samples, match_info_dir, match_timeline_dir):
+    """Fetch num_samples number of match infos and match timelines for each combination of ranks and divisions,
+    saving each match info to a file named 'match_info_[ID].json' and each match timeline to a file named
+    'match_timeline_[ID].json'.
+
+    :param region: String indicating match region
+    :param queue: String indicating match queue type
+    :param ranks: Iterable indicating all ranks to fetch matches from
+    :param divisions: Iterable indicating all rank divisions to fetch matches from
+    :param min_page: Lower bound (inclusive) on the range of pages to randomly choose matches from
+    :param max_page: Upper bound (inclusive) on the range of pages to randomly choose matches from
+    :param num_samples: Number of random matches to fetch from each combination of ranks and divisions
+    :param match_info_dir: Path to the directory to save the match info JSON files to
+    :param match_timeline_dir: Path to the directory to save the match timeline JSON files to
+    :return: None
+    """
 
     lol_watcher = LolWatcher(API_KEY)
 
@@ -39,8 +78,8 @@ def generate_data(region, queue, ranks, divisions, min_page,
                     match_info = fetch_match_info(lol_watcher, region, match_id)
                     match_timeline = fetch_match_timeline(lol_watcher, region, match_id)
 
-                    match_info_filepath = match_info_filepath / ('_' + match_id + '.json')
-                    match_timeline_filepath = match_timeline_filepath / ('_' + match_id + '.json')
+                    match_info_filepath = match_info_dir / ('match_info_' + match_id + '.json')
+                    match_timeline_filepath = match_timeline_dir / ('match_timeline_' + match_id + '.json')
 
                     with match_info_filepath.open(mode='w', encoding='utf-8') as match_info_f:
                         json.dump(match_info, match_info_f, ensure_ascii=False, indent=4)
@@ -62,11 +101,11 @@ def main():
     max_page = 10
     num_samples_per_rank = 20
 
-    match_info_filepath = Path.cwd() / 'matches' / 'match_info'
-    match_timeline_filepath = Path.cwd() / 'matches' / 'match_timeline'
+    match_info_dir = Path.cwd() / 'matches' / 'match_info'
+    match_timeline_dir = Path.cwd() / 'matches' / 'match_timeline'
 
     generate_data(region, queue, non_apex_ranks, divisions,
-                  min_page, max_page, num_samples_per_rank, match_info_filepath, match_timeline_filepath)
+                  min_page, max_page, num_samples_per_rank, match_info_dir, match_timeline_dir)
 
 
 if __name__ == "__main__":
